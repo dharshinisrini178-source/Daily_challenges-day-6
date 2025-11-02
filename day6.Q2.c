b047c3ba85bd4cb06df01ff4288617dc1fbd7566
dharshinisrini178-source <dharshinisrini178@gmail.com>
2025-11-02 19:30:43 +0530

    Add files via upload

---
+++ day6.Q2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() 
+{
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+    if(a >= b && a >= c)
+        printf("Greatest: %d\n", a);
+    else if(b >= a && b >= c)
+        printf("Greatest: %d\n", b);
+	else
+        printf("Greatest: %d\n", c);
+    if(a <= b && a <= c)
+      printf("Smallest: %d", a);
+	else if(b <= a && b <= c)
+        printf("Smallest: %d", b);
+    else
+        printf("Smallest: %d", c);
+    return 0;
+}
